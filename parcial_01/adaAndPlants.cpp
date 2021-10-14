ec2f2acd08880aed201b2d6f5041c37630e70de8
reqhiem <jperca@unsa.edu.pe>
2021-10-14 16:08:59 -0500

    Ejercicio 8

--- parcial_01/adaAndPlants.cpp
+++ parcial_01/adaAndPlants.cpp
@@ -1,25 +1,38 @@
 /**
- *  @descripcion: Este programa NO ES SOLUCION
+ *  @descripcion: Solución y aceptada
 */
 
 #include <iostream>
+#include <set>
 
 using namespace std;
 typedef long long ll;
 
 const int ms = 2e05 + 10;
 
-int c,p,T,N,K, arr[ms];
+int c = 0,p,T,N,K, arr[ms];
 
-void solve(){}
+void solve(){
+    int mf = 0, d;
+    set<pair<int,int>> aux;
+    for(int i = 0; i < N; i++){
+        aux.insert({arr[i],i});
+
+        d = aux.rbegin()->first - aux.begin()->first;
+        if(d > mf) mf = d;
+        if(i-K-1 >= 0) aux.erase({arr[i-K-1],i-K-1});
+    }
+    cout << mf << endl;
+}
 
 int main(int argc, char const *argv[])
 {
     cin >> T;
     while(c++ < T){
+        p = 0;
         cin >> N >> K;
         while(p < N) cin >> arr[p++];
+        solve();
     }
-    solve();
     return 0;
 }
