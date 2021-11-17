2fa6cc17bc072d4bbe1021eecf9c42e7c024dda0
reqhiem <jperca@unsa.edu.pe>
2021-11-17 08:26:36 -0500

    Practica Programacion Dinámica

--- practiceDynamicProg/Tiling.cpp
+++ practiceDynamicProg/Tiling.cpp
@@ -7,12 +7,21 @@ typedef vector<int> vi;
 typedef vector<vector<int>> vvi;
 
 //Tiling problem
-int Tiling(int n){
-    
+int tiling(int n){
+    if (n <= 1) return n;
+    vi dp(n+1, 0);
+    dp[0] = 0;
+    dp[1] = 1;
+
+    for (int i = 2; i <= n; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    return dp[n];
 }
 
 int main(int argc, char const *argv[])
 {
-    cout << Tiling(3) << endl;
+    cout << tiling(4) << endl;
     return 0;
 }
