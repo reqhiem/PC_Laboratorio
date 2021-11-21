ddef2eeac9084a4aed5f1189d75c41ae6c15d0dc
reqhiem <jperca@unsa.edu.pe>
2021-11-21 18:21:14 -0500

    10KindsOfPeople ALMOS SOLVED

--- aula_16/10KindsOfPeople.cpp
+++ aula_16/10KindsOfPeople.cpp
@@ -1,24 +1,32 @@
 #include <iostream>
 #include <string>
+#include <cstring>
 using namespace std;
 
 string line;
-int mat[1000][1000], R, C, N, r1, r2, c1, c2;
+int mat[1005][1005], matTmp[1005][1005];
+int R, C, N, r1, r2, c1, c2;
+bool visited[1005][1005];
+int offsets[4][2] = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
 
-void process(string &line, int i){
-    int j=0;
-    for(auto let : line){
-        mat[i][j++] = (let == '0') ? 0 : 1;
+void dfs(int i, int j, int fc, int fn){
+    visited[i][j] = true;
+    matTmp[i][j] = fn;
+    for(int k = 0; k < 4; k++){
+        int ni = i + offsets[k][0];
+        int nj = j + offsets[k][1];
+        if(ni >= 0 && ni < R && nj >= 0 && nj < C && !visited[ni][nj] && mat[ni][nj] == fc){
+            dfs(ni, nj, fc, fn);
+        }
     }
-}
 
-void query(int r1, int c1, int r2, int c2){
-    cout << "binary" << endl;
 }
 
 int main(int argc, char const *argv[]){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
+    cout.tie(NULL);
+    memset(visited, false, sizeof visited);
 
     cin >> R >> C;
 
@@ -28,17 +36,27 @@ int main(int argc, char const *argv[]){
         for(auto car : line) mat[i][j++] = (car == '0') ? 0 : 1;
     }
 
+    int cont = 0;
+    //Process
     for(int i=0; i<R; i++){
         for(int j=0; j<C; j++){
-            cout << mat[i][j] << " ";
+            if(!visited[i][j]){
+                dfs(i,j,mat[i][j],cont);
+                cont++;
+            }
         }
-        cout << endl;
     }
 
+
+    //Queries
     cin >> N;
     for(int i=0; i<N; i++){
         cin >> r1 >> c1 >> r2 >> c2;
-        query(r1, c1, r2, c2);
+        r1--; c1--; r2--; c2--;
+        if(matTmp[r1][c1] == matTmp[r2][c2]){
+            if(mat[r1][c1] == 0) cout << "binary\n";
+            else cout << "decimal\n";
+        }else cout << "neither\n";
     }
 
     return 0;
