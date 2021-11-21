0d7945c7fcafcb19775b6ed0ddfd70f359830561
reqhiem <jperca@unsa.edu.pe>
2021-11-21 09:05:12 -0500

    Flood Fill problem from LeetCode

--- aula_15/floodFill.cpp
+++ aula_15/floodFill.cpp
@@ -9,20 +9,30 @@ using namespace std;
 static int offsets[][2] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
 
 void _floodFill(vector<vector<int>> &grid, int i, int j, int color, int oldCol){
-    if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size() || grid[i][j] != oldCol)
+    if(i < 0 || i >= grid.size() || j < 0 || j >= grid[0].size())
         return;
-    grid[i][j] = color;
+    if(grid[i][j] != oldCol) return;
 
+    grid[i][j] = color;
     for(int k=0; k<OSIZE; ++k){
         _floodFill(grid, i+offsets[k][0], j+offsets[k][1], color, oldCol);
     }
 }
 
+vector<vector<int>> floodFill(vector<vector<int>> &grid, int i, int j, int color){
+    if(grid[i][j] == color) return grid;
+
+    _floodFill(grid, i, j, color, grid[i][j]);
+    return grid;
+}
+
+
+
+
 int main(int argc, char const *argv[])
 {
-    vector<vector<int>> grid = {{1,1,1},
-                                {1,1,0},
-                                {1,0,1}};
+    //vector<vector<int>> grid = {{1, 1, 1}, {1, 1, 0}, {1, 0, 1}};
+    vector<vector<int>> grid = {{0,0,0},{0,1,1}};
     cout << "Original Grid:\n";
     for(auto i : grid){
         for(auto j : i)
@@ -30,10 +40,10 @@ int main(int argc, char const *argv[])
         cout << endl;
     }
 
-    _floodFill(grid, 1, 1, 2, 1);
-
     cout << "Flood fill Grid:\n";
-    for(auto i : grid){
+    vector<vector<int>> newGrid = floodFill(grid, 1, 1, 2);
+
+    for(auto i : newGrid){
         for(auto j : i)
             cout << j << " ";
         cout << endl;
